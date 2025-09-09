f675fc25e7c6ffb72e1aa649b8fc3fcb0786792d
jeyjunior <jeyjuniordg@gmail.com>
2025-09-08 21:35:46 -0300

    Modelo Teste Sincrono para Download Automatico de Arquivos

---
+++ WinForms_DownloadFileAuto/Form1.Designer.cs
@@ -0,0 +1,145 @@
+﻿namespace WinForms_DownloadFileAuto
+{
+    partial class Principal
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle10 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle11 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle12 = new System.Windows.Forms.DataGridViewCellStyle();
+            this.btnTestar = new System.Windows.Forms.Button();
+            this.dtgArquivos = new System.Windows.Forms.DataGridView();
+            this.txtDiretorioDestino = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.colSequencia = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.colNome = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.colTotal = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dtgArquivos)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnTestar
+            // 
+            this.btnTestar.Location = new System.Drawing.Point(647, 12);
+            this.btnTestar.Name = "btnTestar";
+            this.btnTestar.Size = new System.Drawing.Size(141, 42);
+            this.btnTestar.TabIndex = 0;
+            this.btnTestar.Text = "Testar";
+            this.btnTestar.UseVisualStyleBackColor = true;
+            this.btnTestar.Click += new System.EventHandler(this.btnTestar_Click);
+            // 
+            // dtgArquivos
+            // 
+            this.dtgArquivos.AllowUserToAddRows = false;
+            this.dtgArquivos.AllowUserToDeleteRows = false;
+            this.dtgArquivos.AllowUserToResizeColumns = false;
+            this.dtgArquivos.AllowUserToResizeRows = false;
+            this.dtgArquivos.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dtgArquivos.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.colSequencia,
+            this.colNome,
+            this.colTotal});
+            this.dtgArquivos.EnableHeadersVisualStyles = false;
+            this.dtgArquivos.Location = new System.Drawing.Point(12, 75);
+            this.dtgArquivos.Name = "dtgArquivos";
+            this.dtgArquivos.RowHeadersVisible = false;
+            this.dtgArquivos.Size = new System.Drawing.Size(776, 349);
+            this.dtgArquivos.TabIndex = 1;
+            // 
+            // txtDiretorioDestino
+            // 
+            this.txtDiretorioDestino.Location = new System.Drawing.Point(12, 34);
+            this.txtDiretorioDestino.Name = "txtDiretorioDestino";
+            this.txtDiretorioDestino.Size = new System.Drawing.Size(605, 20);
+            this.txtDiretorioDestino.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 12);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(98, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Diretório de destino";
+            // 
+            // colSequencia
+            // 
+            this.colSequencia.DataPropertyName = "Sequencia";
+            dataGridViewCellStyle10.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter;
+            this.colSequencia.DefaultCellStyle = dataGridViewCellStyle10;
+            this.colSequencia.HeaderText = "Sequencia";
+            this.colSequencia.MinimumWidth = 80;
+            this.colSequencia.Name = "colSequencia";
+            this.colSequencia.Width = 80;
+            // 
+            // colNome
+            // 
+            this.colNome.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.colNome.DataPropertyName = "Nome";
+            dataGridViewCellStyle11.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            this.colNome.DefaultCellStyle = dataGridViewCellStyle11;
+            this.colNome.HeaderText = "Nome";
+            this.colNome.Name = "colNome";
+            // 
+            // colTotal
+            // 
+            this.colTotal.DataPropertyName = "Total";
+            dataGridViewCellStyle12.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter;
+            this.colTotal.DefaultCellStyle = dataGridViewCellStyle12;
+            this.colTotal.HeaderText = "Total";
+            this.colTotal.MinimumWidth = 100;
+            this.colTotal.Name = "colTotal";
+            // 
+            // Principal
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtDiretorioDestino);
+            this.Controls.Add(this.dtgArquivos);
+            this.Controls.Add(this.btnTestar);
+            this.Name = "Principal";
+            this.Text = "Download File Auto";
+            this.Load += new System.EventHandler(this.Principal_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dtgArquivos)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnTestar;
+        private System.Windows.Forms.DataGridView dtgArquivos;
+        private System.Windows.Forms.TextBox txtDiretorioDestino;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn colSequencia;
+        private System.Windows.Forms.DataGridViewTextBoxColumn colNome;
+        private System.Windows.Forms.DataGridViewTextBoxColumn colTotal;
+    }
+}
+
---
+++ WinForms_DownloadFileAuto/Form1.cs
@@ -0,0 +1,203 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Net.WebRequestMethods;
+
+namespace WinForms_DownloadFileAuto
+{
+    public partial class Principal : Form
+    {
+        private List<string> Arquivos = new List<string>()
+        {
+            "https://archivepublicdomain.com/files/2025/06/A-ARTE-DA-GUERRA.pdf",
+            "https://multimidia.gazetadopovo.com.br/media/info/2021/202110/bichos/revolucao-dos-bichos.pdf",
+            "https://livraria-camara-leg.usrfiles.com/ugd/5ca0e9_77426ca451ec4f60b14af67f925f038e.pdf",
+            "https://eniopadilha-com-br.usrfiles.com/ugd/5ca0e9_25f5954b7b7a4a76892d3650ec0cd36c.pdf",
+            "https://www.ebooksbrasil.org/adobeebook/alicep.pdf",
+            "https://archivepublicdomain.com/files/2025/02/metamorfose.pdf",
+            "https://archivepublicdomain.com/files/2025/02/O-Alienista.pdf",
+            "https://camara-leg-br.usrfiles.com/ugd/5ca0e9_4bd76b96516a40fba87370e6bcec72f8.pdf",
+            "https://www.baixelivros.com.br/ciencias-humanas-e-sociais/filosofia/o-banquete-platao",
+            "https://5ca0e999-de9a-47e0-9b77-7e3eeab0592c.usrfiles.com/ugd/5ca0e9_4f0243f83ea945ed98bfa7570d3b743c.pdf",
+        };
+        private List<Item> Itens = new List<Item>();
+        private HttpClient httpClient;
+        public Principal()
+        {
+            InitializeComponent();
+
+            httpClient = new HttpClient();
+            httpClient.Timeout = TimeSpan.FromMinutes(30);
+        }
+        private void Principal_Load(object sender, EventArgs e)
+        {
+            var random = new Random();
+
+            for (int i = 1; i <= 10; i++)
+            {
+                var item = new Item
+                {
+                    Sequencia = i,
+                    Nome = "Teste " + i,
+                    Arquivos = new List<string>(),
+                    ArquivoAtual = 0,
+                };
+
+                item.TotalArquivos = random.Next(3, Arquivos.Count + 1);
+
+                for (int j = 0; j < item.TotalArquivos; j++)
+                    item.Arquivos.Add(Arquivos[j]);
+
+                Itens.Add(item);
+            }
+
+            Bind();
+        }
+
+        private void Bind()
+        {
+            dtgArquivos.DataSource = Itens.Select(c => new
+            {
+                Sequencia = c.Sequencia,
+                Nome = c.Nome,
+                Total = $"{c.ArquivoAtual}/{c.TotalArquivos}",
+                Status = c.Status
+            }).ToList();
+        }
+
+        private async void btnTestar_Click(object sender, EventArgs e)
+        {
+            string diretorio = txtDiretorioDestino.Text;
+
+            if (string.IsNullOrWhiteSpace(diretorio))
+            {
+                MessageBox.Show("Por favor, informe o diretório de destino.");
+                return;
+            }
+
+            if (!Directory.Exists(diretorio))
+            {
+                MessageBox.Show("Diretório de destino não existe.");
+                return;
+            }
+
+            btnTestar.Enabled = false;
+            btnTestar.Text = "Baixando...";
+
+            try
+            {
+                await ProcessarDownloads(diretorio);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Erro durante o download: {ex.Message}");
+            }
+            finally
+            {
+                btnTestar.Enabled = true;
+                btnTestar.Text = "Download";
+            }
+        }
+
+        private async Task ProcessarDownloads(string diretorioBase)
+        {
+            foreach (var item in Itens)
+            {
+                item.Status = "Iniciando...";
+                Bind();
+
+                string diretorioItem = Path.Combine(diretorioBase, item.Nome);
+
+                if (!Directory.Exists(diretorioItem))
+                {
+                    Directory.CreateDirectory(diretorioItem);
+                }
+
+                for (int i = 0; i < item.Arquivos.Count; i++)
+                {
+                    string url = item.Arquivos[i];
+                    string nomeArquivo = ObterNomeArquivoFromUrl(url);
+                    string caminhoCompleto = Path.Combine(diretorioItem, nomeArquivo);
+
+                    item.Status = $"Baixando {i + 1}/{item.TotalArquivos}";
+                    Bind();
+
+                    try
+                    {
+                        await BaixarArquivo(url, caminhoCompleto);
+                        item.ArquivoAtual = i + 1;
+                        item.Status = "Sucesso";
+                    }
+                    catch (Exception ex)
+                    {
+                        item.Status = $"Erro: {ex.Message}";
+                        break; 
+                    }
+
+                    Bind();
+                    await Task.Delay(100); 
+                }
+
+                Bind();
+            }
+        }
+
+        private async Task BaixarArquivo(string url, string caminhoDestino)
+        {
+            try
+            {
+                using (var response = await httpClient.GetAsync(url, HttpCompletionOption.ResponseHeadersRead))
+                {
+                    response.EnsureSuccessStatusCode();
+
+                    using (var stream = await response.Content.ReadAsStreamAsync())
+                    using (var fileStream = new FileStream(caminhoDestino, FileMode.Create, FileAccess.Write, FileShare.None))
+                    {
+                        await stream.CopyToAsync(fileStream);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                throw new Exception($"Erro ao baixar {url}: {ex.Message}");
+            }
+        }
+
+        private string ObterNomeArquivoFromUrl(string url)
+        {
+            try
+            {
+                Uri uri = new Uri(url);
+                return Path.GetFileName(uri.LocalPath);
+            }
+            catch
+            {
+                return $"arquivo_{DateTime.Now:yyyyMMddHHmmssfff}.download";
+            }
+        }
+
+        protected override void OnFormClosing(FormClosingEventArgs e)
+        {
+            httpClient?.Dispose();
+            base.OnFormClosing(e);
+        }
+    }
+}
+
+public class Item
+{
+    public int Sequencia { get; set; }
+    public string Nome { get; set; }
+    public int TotalArquivos { get; set; }
+    public int ArquivoAtual { get; set; }
+    public string Status { get; set; } = "Pendente";
+    public List<string> Arquivos { get; set; }
+}
